bac3831f52442b4efe8bfb437e2a72d2891ccde8
taaweezin00 <119288371+taaweezin00@users.noreply.github.com>
2023-04-01 03:24:28 +0700

    Add files via upload

---
+++ project หนัง/project หนัง/MovieDB/สุ่มหนัง(encapsulation and information hiding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieDB
+{
+    public class RandomWordGenerator
+    {
+        private readonly string[] _words;
+        private readonly Random _random;
+
+        public RandomWordGenerator(string[] words)
+        {
+            _words = words;
+            _random = new Random();
+        }
+
+        public string Generate()
+        {
+            int index = _random.Next(_words.Length);
+            return _words[index];
+        }
+    }
+}
